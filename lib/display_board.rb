59ea24e2e265cc8d74737129f395a9a9ba218211
valentine-m@live.fr <valentine-m@live.fr>
2018-07-20 18:00:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-string = 3.times "   " |
-separation = 11.times -
+string = 3.times "   " "|"
+separation = 11.times "-"
 puts string
 puts separation
 puts string
